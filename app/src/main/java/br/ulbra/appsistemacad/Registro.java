184ba8b057eb2b711ef5aee55fdd0043430304e6
oIgorTorres <igor.td29@gmail.com>
2025-10-07 21:14:12 -0300

    v1

---
+++ app/src/main/java/br/ulbra/appsistemacad/Registro.java
@@ -0,0 +1,34 @@
+package br.ulbra.appsistemacad;
+
+public class Registro {
+        private String nome;
+        private String endereco;
+        private String telefone;
+
+
+        // Construtor: usado para criar um novo objeto Registro com os dados iniciais
+        public Registro(String nome, String endereco, String telefone) {
+
+            this.nome = nome;
+            this.endereco = endereco;
+            this.telefone = telefone;
+        }
+
+
+        // Métodos "get": usados para acessar os dados armazenados no objeto
+        public String getNome() {
+            return nome;
+        }
+
+
+        public String getEndereco() {
+            return endereco;
+        }
+
+
+        public String getTelefone() {
+            return telefone;
+        }
+    }
+
+
